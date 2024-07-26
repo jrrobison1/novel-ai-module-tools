bb99759627fa3c5a364ffad6ec2540eeaae817ea
Jason Robison <jrrobison1@hoodsen.me>
2024-07-26 14:17:35 -0600

    Refactor resources_loader

--- novel_ai_module_tools/resources_loader.py
+++ novel_ai_module_tools/resources_loader.py
@@ -1,48 +1,63 @@
-from collections import OrderedDict
 import logging
-import os
-from os import walk
+from collections import OrderedDict
+from pathlib import Path
 import random
 
 logger = logging.getLogger(__name__)
 
 
 def load_names(path):
+    """
+    Load names from files in the specified directory.
+
+    Args:
+        path (Path): The directory path containing name files.
+
+    Returns:
+        OrderedDict: A dictionary where keys are name types (based on filenames)
+                     and values are shuffled lists of names.
+    """
     logger.info(f"Loading names from path: [{path}]")
     name_replacements = OrderedDict()
-    file_names = next(walk(path), (None, None, []))[2]  # [] if no file
-    file_names.sort()
+    file_names = sorted([f.name for f in path.iterdir() if f.is_file()])
 
     for filename in file_names:
         logger.info(f"Loading names from file: [{filename}]")
         if filename.startswith("."):
             continue
 
-        name_type = os.path.splitext(filename)[0]
+        name_type = Path(filename).stem
 
-        with open(os.path.join(path, filename)) as f:
+        with open(path / filename) as f:
             name_replacements[name_type] = f.read().splitlines()
             random.shuffle(name_replacements[name_type])
-        f.close()
 
     return name_replacements
 
 
 def load_name_replacements():
-    resource_directory = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "resources"
-    )
-    names_directory = os.path.join(resource_directory, "names")
-    replacements_directory = os.path.join(names_directory, "replace")
+    """
+    Load name replacements from the 'replace' directory.
+
+    Returns:
+        OrderedDict: A dictionary of name replacements.
+    """
+    resource_directory = Path(__file__).parent / "resources"
+    names_directory = resource_directory / "names"
+    replacements_directory = names_directory / "replace"
 
     return load_names(replacements_directory)
 
 
 def load_name_recognizers():
-    resource_directory = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "resources"
-    )
-    names_directory = os.path.join(resource_directory, "names")
-    recognizers_directory = os.path.join(names_directory, "recognize")
+    """
+    Load name recognizers from the 'recognize' directory.
+
+    Returns:
+        OrderedDict: A dictionary of name recognizers.
+    """
+    resource_directory = Path(__file__).parent / "resources"
+    names_directory = resource_directory / "names"
+    recognizers_directory = names_directory / "recognize"
 
     return load_names(recognizers_directory)
